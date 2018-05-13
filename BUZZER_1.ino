9036af91ea6aef7f590358747688edbce4d13fc1
MarRosado <39241831+MarRosado@users.noreply.github.com>
2018-05-13 14:05:03 -0600

    Add files via upload

---
+++ BUZZER_1.ino
@@ -0,0 +1,16 @@
+int BUZZER=13;
+int SENSOR=10;
+void setup(){
+  pinMode(BUZZER,OUTPUT);
+  pinMode(SENSOR,INPUT);
+  digitalWrite(BUZZER,LOW);
+}
+
+void loop(){
+  if (digitalRead(SENSOR)**LOW){
+    digitalWrite(BUZZER,HIGH);
+    delay (2000);
+  }else{
+    digitalWrite(BUZZER,LOW);
+  }
+   }
---
+++ arbol_bueno_fin.ino
@@ -0,0 +1,21 @@
+int Led=13;
+int Sensor=10;
+
+
+void setup() {
+pinMode(Led,OUTPUT);
+pinMode(Sensor,INPUT);
+digitalWrite(Led,LOW);
+}
+
+
+void loop(){
+if(digitalRead(Sensor)==HIGH){
+  digitalWrite(Led,HIGH);
+  delay (2000);
+}else{
+  digitalWrite(Led,LOW);
+}
+}
+
+
